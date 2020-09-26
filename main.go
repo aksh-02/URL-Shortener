71b2fa3bec7f10854666e927ab4b5930d9e371e4
aksh-02 <aksh0299@gmail.com>
2020-09-26 09:37:18 +0530

    Added Database

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/md5"
 	"encoding/hex"
 	"encoding/json"
@@ -12,15 +13,33 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MyURL struct {
 	URL string `json="url"`
 }
 
-var URLDict = make(map[string]string)
-
 var alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func dbClient(ctx context.Context) (*mongo.Client, error) {
+	uri := ""
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		fmt.Printf("Couldn't connect to the cluster : %v\n", err)
+		return nil, err
+	}
+
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
 
 func handleCollision(n int, s string) string {
 	return s[:len(s)-1] + string(alphabet[n])
@@ -52,6 +71,7 @@ func Shortener(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&p)
 	if err != nil {
 		fmt.Printf("Couldn't decoce URL : %v\n", err)
+		return
 	}
 	hash := md5.Sum([]byte(p.URL))
 	strHash := hex.EncodeToString(hash[:])[:10] // First 40 bits out of the 128 bits should be enough
@@ -62,16 +82,28 @@ func Shortener(w http.ResponseWriter, r *http.Request) {
 	}
 	b62val := encode(val) // encoding in base62
 	for {
-		_, ok := URLDict[b62val] // If the shortened URL already exists i.e collision, get new URL
-		if ok {
+		longURL := bson.M{}
+		if err = collection.FindOne(ctx, bson.M{"surl": b62val}).Decode(longURL); err == nil {
 			fmt.Printf("%v already exists in the database.\n", b62val)
-			b62val = handleCollision(rand.Intn(62), b62val)
+			lURL := longURL["lurl"]
+			if lURL == p.URL {
+				fmt.Println("Shortened URL already exists for this URL")
+				break
+			}
+			b62val = handleCollision(rand.Intn(62), b62val) // If the shortened URL already exists i.e collision, get new URL
 		} else {
+			_, err = collection.InsertOne(ctx, bson.M{"surl": b62val, "lurl": p.URL})
+			if err != nil {
+				resp := fmt.Sprintf("Failed to insert document : %v", err)
+				fmt.Println(resp)
+				err = json.NewEncoder(w).Encode(resp)
+				return
+			}
 			break
 		}
 	}
-	URLDict[b62val] = p.URL
-	fmt.Println("Shortened", b62val, URLDict[b62val])
+
+	fmt.Println("Base62 & LongURL", b62val, p.URL)
 	resp := fmt.Sprintf("Your shortened URL is : %v/%v", r.Host, b62val)
 	fmt.Println(resp)
 	err = json.NewEncoder(w).Encode(resp)
@@ -84,22 +116,46 @@ func Shortener(w http.ResponseWriter, r *http.Request) {
 func Expander(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-	longURL, ok := URLDict[id]
-	if !ok {
-		w.Write([]byte("Shortened URL incorrect."))
+
+	longURL := bson.M{}
+	if err = collection.FindOne(ctx, bson.M{"surl": id}).Decode(longURL); err != nil {
+		resp := fmt.Sprintf("Your URL doesn't exist yet : %v", err)
+		fmt.Println(resp)
+		err = json.NewEncoder(w).Encode(resp)
 		return
 	}
-	fmt.Println("longURL :", longURL)
-	http.Redirect(w, r, longURL, http.StatusSeeOther)
+	lURL := longURL["lurl"]
+	fmt.Println("longURL :", lURL)
+	strURL, _ := lURL.(string)
+	http.Redirect(w, r, strURL, http.StatusSeeOther)
 }
 
+var ctx context.Context
+var client *mongo.Client
+var err error
+var collection *mongo.Collection
+
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err = dbClient(ctx)
+	if err != nil {
+		fmt.Printf("Some error in database connection %v\n", err)
+	}
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	collection = client.Database("URLService").Collection("Shortened")
+
 	sm := mux.NewRouter()
 	sm.HandleFunc("/{id:[a-zA-Z0-9]+}", Expander).Methods(http.MethodGet)
 	sm.HandleFunc("/", Shortener).Methods(http.MethodPost)
 
 	s := http.Server{
-		Addr:         ":8080",          // configure the bind address
+		Addr:         "127.0.0.1:8080", // configure the bind address
 		Handler:      sm,               // set the default handler
 		ReadTimeout:  5 * time.Second,  // max time to read request from the client
 		WriteTimeout: 10 * time.Second, // max time to write response to the client
